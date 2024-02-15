848ed7a72f9056c2b605f8b90906d6dddce6fee8
Shim Jae Chun <shimnaldo98@gmail.com>
2024-02-15 17:51:40 +0900

    Feat : Develop commit

--- Assets/02. Scripts/Utils/StateMachine.cs
+++ Assets/02. Scripts/Utils/StateMachine.cs
@@ -2,18 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StateMachine
+public class StateMachine<TState>
 {
+    private Dictionary<TState, IState> states = new Dictionary<TState, IState>();
+    public Dictionary<TState, IState> States { get { return states; } }
+    
     public IState curState;
 
     public void Update()
     {
         curState.Update();
     }
-    public void ChangeState(IState nextState)
+
+    public void SetInitState(TState type)
+    {
+        curState = states[type];
+        curState.Enter();
+    }
+    public void ChangeState(TState type)
     {
         curState.Exit();
-        curState = nextState;
+        curState = states[type];
         curState.Enter();
+    }
+    public void AddState(TState type, IState state)
+    {
+        states.Add(type, state);
     }
 }
